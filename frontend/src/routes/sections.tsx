1aa17778e4e30a23077c1d6b3c87f1196e1278fd
kingdomax <kingdomax@windowslive.com>
2025-04-10 23:05:31 +0200

    Add nginx config to support SPA

--- frontend/src/routes/sections.tsx
+++ frontend/src/routes/sections.tsx
@@ -67,5 +67,8 @@ export const routesSection: RouteObject[] = [
         path: '404',
         element: <Page404 />,
     },
-    { path: '*', element: <Page404 /> },
+    {
+        path: '*',
+        element: <Page404 />,
+    },
 ];
