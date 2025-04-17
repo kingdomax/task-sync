8f8c88dfcae5e457d7e74928cbd1a1c7c887a37a
kingdomax <kingdomax@windowslive.com>
2025-04-17 22:08:37 +0200

    scalfold for CRUD's project and task

---
+++ backend/Repositories/TasksRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskSync.Repositories
+{
+    public class TasksRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public TasksRepository(AppDbContext dbContext) => _dbContext = dbContext;
+    }
+}
---
+++ backend/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskSync.Services.Interfaces
+{
+    public interface ITaskService
+    {
+    }
+}
