4405b9a8029139272b4ecb050b1db2c141a741ba
kingdomax <kingdomax@windowslive.com>
2025-05-15 20:29:24 +0200

    add delete item loginc in clientside

--- frontend/src/sections/kanbanboard/view/kanban-board-view.tsx
+++ frontend/src/sections/kanbanboard/view/kanban-board-view.tsx
@@ -68,6 +68,8 @@ export const KanbanBoardView = () => {
                     setKanbanItems((prev) =>
                         prev.map((item) => (item.id === response.data.id ? response.data : item))
                     );
+                } else if (response.status == NOTIFY_STATUS.DELETE) {
+                    setKanbanItems((prev) => prev.filter((item) => item.id !== response.data.id));
                 }
             });
 
@@ -82,47 +84,6 @@ export const KanbanBoardView = () => {
             connectionRef.current?.stop();
         };
     }, []);
-
-    const handleStatusChange = async (data: TaskDto, newStatus: TASK_STATUS) => {
-        // Optimistically update UI
-        setKanbanItems((prev) =>
-            prev.map((item) =>
-                item.id === data.id
-                    ? { ...item, status: newStatus, lastModified: new Date() }
-                    : item
-            )
-        );
-
-        // Then send request to server
-        try {
-            const res = await fetch(`${getApiUrl()}/task/updateStatus/${data.id}`, {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-connection-id': connectionIdRef.current,
-                },
-                body: JSON.stringify({ statusRaw: newStatus }),
-            });
-
-            if (!res.ok) {
-                throw new Error();
-            }
-
-            const updated = await res.json();
-            setKanbanItems((prev) => prev.map((item) => (item.id === updated.id ? updated : item)));
-        } catch (err) {
-            // Revert on failure
-            setKanbanItems((prev) =>
-                prev.map((item) =>
-                    item.id === data.id
-                        ? { ...item, status: data.status, lastModified: data.lastModified }
-                        : item
-                )
-            );
-            // Optional: toast / error message
-            console.log(err);
-        }
-    };
 
     const handleAddItem = async (
         newItem: AddItemRequest,
@@ -152,6 +113,65 @@ export const KanbanBoardView = () => {
             } else {
                 onFailure(`unknown error: ${err}`);
             }
+        }
+    };
+
+    const handleStatusChange = async (data: TaskDto, newStatus: TASK_STATUS) => {
+        // Optimistically update UI
+        setKanbanItems((prev) =>
+            prev.map((item) =>
+                item.id === data.id
+                    ? { ...item, status: newStatus, lastModified: new Date() }
+                    : item
+            )
+        );
+
+        // Then send request to server
+        try {
+            const res = await fetch(`${getApiUrl()}/task/updateStatus/${data.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-connection-id': connectionIdRef.current,
+                },
+                body: JSON.stringify({ statusRaw: newStatus }),
+            });
+
+            if (!res.ok) {
+                throw new Error();
+            }
+
+            const updated = await res.json();
+            setKanbanItems((prev) => prev.map((item) => (item.id === updated.id ? updated : item)));
+        } catch (err: any) {
+            // Revert on failure
+            setKanbanItems((prev) =>
+                prev.map((item) =>
+                    item.id === data.id
+                        ? { ...item, status: data.status, lastModified: data.lastModified }
+                        : item
+                )
+            );
+            // Optional: toast / error message
+            console.log(err);
+        }
+    };
+
+    const handleDeleteItem = async (deleteItem: TaskDto) => {
+        setKanbanItems((prev) => prev.filter((item) => item.id !== deleteItem.id));
+
+        try {
+            const res = await fetch(`${getApiUrl()}/task/deleteTasks/${deleteItem.id}`, {
+                method: 'DELETE',
+                headers: { 'x-connection-id': connectionIdRef.current },
+            });
+
+            if (!res.ok) {
+                throw new Error();
+            }
+        } catch (err) {
+            setKanbanItems([...kanbanItems, deleteItem]); // Revert on failure
+            alert(err);
         }
     };
 
@@ -194,6 +214,7 @@ export const KanbanBoardView = () => {
                                     color={statusColors[status]}
                                     data={item}
                                     onStatusChange={handleStatusChange}
+                                    onDelete={handleDeleteItem}
                                 />
                             ))}
                         </Grid>
