06f48a6054ca31e694a49e1fea77e900a65841ba
kingdomax <kingdomax@windowslive.com>
2025-05-23 21:33:12 +0200

    Move all infra component to register at global level

--- gamification-api/src/main.ts
+++ gamification-api/src/main.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { AuthGuard } from './common/guards/auth.guard';
 import { TimingInterceptor } from './common/interceptors/timing.interceptor';
 import { HttpExceptionFilter } from './common/exceptionfilters/http-exception.filter';
+import { RequestLoggerMiddleware } from './common/middlewares/request-logger.middleware';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.use(TimingInterceptor);
-    app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // enable DTO validation globally
+    app.use(new RequestLoggerMiddleware()); // only place to register middleware, cannot register per route via @attribution
+
+    app.useGlobalGuards(new AuthGuard());
+    app.useGlobalInterceptors(new TimingInterceptor());
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     app.useGlobalFilters(new HttpExceptionFilter());
 
     await app.listen(process.env.PORT ?? 3000);
--- gamification-api/src/points/points.controller.ts
+++ gamification-api/src/points/points.controller.ts
@@ -1,18 +1,8 @@
-import {
-    Body,
-    Controller,
-    Post,
-    UseGuards,
-    UseInterceptors,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PointsService } from './points.service';
 import { CreatePointDto } from './dto/create-point.dto';
-import { TimingInterceptor } from 'src/common/interceptors/timing.interceptor';
-import { AuthGuard } from 'src/common/guards/auth.guard';
 
 @Controller('points')
-// @UseGuards(AuthGuard) todo-moch: move to global
-// @UseInterceptors(TimingInterceptor)
 export class PointsController {
     constructor(private readonly pointsService: PointsService) {}
 
