d6d8d7a6031973fd63767569f2ecbe35ed333ec3
kingdomax <kingdomax@windowslive.com>
2025-06-07 23:10:39 +0200

    make points controller work (temp)

--- gamification-api/src/common/middlewares/request-logger.middleware.ts
+++ gamification-api/src/common/middlewares/request-logger.middleware.ts
@@ -1,12 +1,22 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+//import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
-@Injectable()
-export class RequestLoggerMiddleware implements NestMiddleware {
-    use(req: Request, res: Response, next: NextFunction) {
-        console.log(
-            `[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`
-        );
-        next();
-    }
+//@Injectable() need to inject at module
+//export class RequestLoggerMiddleware implements NestMiddleware {
+//    use(req: Request, res: Response, next: NextFunction) {
+//        console.log(
+//            `[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`
+//        );
+//        next();
+//    }
+//}
+
+export function requestLoggerMiddleware(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    console.log(`-------------------`);
+    console.log(`[RequestLoggerMiddleware] ${req.method} ${req.originalUrl}`);
+    next();
 }
